bff7d5e3f144e0163e8a752576626506f5ed15ec
xuming <shibing624@126.com>
2017-02-12 10:57:42 +0800

    更新pom xuming 20170212

--- src/main/java/org/xm/Case.java
+++ src/main/java/org/xm/Case.java
@@ -1,8 +1,16 @@
 package org.xm;
 
+import org.xm.xmnlp.Xmnlp;
+import org.xm.xmnlp.seg.domain.Term;
+
+import java.util.List;
+
 /**
  * @author xuming
  */
 public class Case {
-
+    public static void main(String[] args) {
+        List<Term> list = Xmnlp.segment("你好外国人");
+        System.out.println(list);
+    }
 }
